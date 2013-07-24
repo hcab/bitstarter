dfb83ba7de88d8cb9b9f357be58bb2ca739a60cd
Ubuntu <ubuntu@ip-172-31-38-218.us-west-2.compute.internal>
2013-07-24 06:30:36 +0000

    some changes

--- grader.js
+++ grader.js
@@ -26,6 +26,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('./restler');
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +37,18 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var assertRemoteFileExists = function(infile){
+    var instr = infile.toString();
+    rest.get(instr).on('complete', function(result) {
+  if (result instanceof Error) {
+    console.log('Error: ' + result.message);
+  } else {
+	console.log(result.toString())
+  }
+});
+
+}
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -65,6 +79,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url>', 'Path to json on the web', clone(assertRemoteFileExists), HTMLFILE_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
